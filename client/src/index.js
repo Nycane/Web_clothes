619e0e3e705bbb1de0edeae9dd899a87c7b27e65
doit-now <hung0925068118@gmail.com>
2023-07-16 23:12:29 +0700

    update

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
         <GlobalStyles>
             <Provider store={store}>
                 <PersistGate loading={null} persistor={persistor}>
@@ -19,7 +19,7 @@ root.render(
                 </PersistGate>
             </Provider>
         </GlobalStyles>
-      </React.StrictMode>,
+    //   </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
